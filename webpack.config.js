e29a5c9821cf683ae5fbbcb412d4839ee9a10bfa
YelKar <karamyshev_e@1060.ru>
2025-02-28 15:21:20 +0300

    Добавил webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './script.js', // Главный файл
+    output: {
+        filename: 'module.js', // Имя выходного JS-файла
+        path: path.resolve(__dirname, ''), // Папка для результата
+    },
+    mode: 'production', // Режим production для минификации
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Обрабатываем все .js файлы
+                exclude: /node_modules/, // Исключаем папку node_modules
+                type: 'javascript/auto',
+                use: {
+                    loader: 'babel-loader', // Используем babel-loader
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/, // Обрабатываем все .css файлы
+                use: [
+                    'style-loader', // Встраивает CSS в <style> теги
+                    'css-loader', // Загружает CSS
+                ],
+            },
+        ],
+    },
+};
